363bdc8a8966cb028eb8fcf6a42e49e6367037f2
NamanPrakash99 <namanprakash2580@gmail.com>
2026-04-14 16:57:03 +0530

    fixed issues

--- auth-service/src/test/java/com/group2/auth_service/dto/DtoTest.java
+++ auth-service/src/test/java/com/group2/auth_service/dto/DtoTest.java
@@ -13,17 +13,6 @@ class DtoTest {
         assertEquals(1L, dto.getId());
         assertEquals("user", dto.getName());
         assertEquals("ROLE_USER", dto.getRole());
-
-        dto.setToken("newT");
-        dto.setRefreshToken("newR");
-        dto.setId(2L);
-        dto.setRole("ROLE_ADMIN");
-        dto.setName("admin");
-        assertEquals("newT", dto.getToken());
-        assertEquals("newR", dto.getRefreshToken());
-        assertEquals(2L, dto.getId());
-        assertEquals("ROLE_ADMIN", dto.getRole());
-        assertEquals("admin", dto.getName());
     }
 
     @Test
@@ -41,32 +30,6 @@ class DtoTest {
         dto.setName("user");
         dto.setEmail("e");
         dto.setPassword("p");
-        dto.setPhone("1");
-        dto.setAddress("a");
         assertEquals("user", dto.getName());
-        assertEquals("e", dto.getEmail());
-        assertEquals("p", dto.getPassword());
-        assertEquals("1", dto.getPhone());
-        assertEquals("a", dto.getAddress());
-    }
-
-    @Test
-    void testResetPasswordRequest() {
-        ResetPasswordRequest dto = new ResetPasswordRequest();
-        dto.setToken("t");
-        dto.setNewPassword("p");
-        assertEquals("t", dto.getToken());
-        assertEquals("p", dto.getNewPassword());
-    }
-
-    @Test
-    void testUpdateProfileRequest() {
-        UpdateProfileRequest dto = new UpdateProfileRequest();
-        dto.setName("n");
-        dto.setPhone("p");
-        dto.setAddress("a");
-        assertEquals("n", dto.getName());
-        assertEquals("p", dto.getPhone());
-        assertEquals("a", dto.getAddress());
     }
 }
